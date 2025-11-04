cc3da829cf05e9f448fe52dce2aaa9ac89bbc7ec
Kirill.TSS <tsekov.k@bcharge.ru>
2025-11-04 14:56:52 +0500

    A Revert "first commit"
    TOAhis reverts commit ed81580b87b016160ac977893377d5f4d0fc83f9.

--- src/model/Meat.java
+++
@@ -1,9 +0,0 @@
-package model;
-
-public class Meat extends Food {
-     public Meat(int amount, double price) {
-         super(amount, price, false);
-     }
-
-
-}
--- src/model/constants/Discount.java
+++
@@ -1,4 +0,0 @@
-package model.constants;
-
-public class Discount { public static final double DISCOUNT_FOR_RED_APPLES = 60.0;
-}
